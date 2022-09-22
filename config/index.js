30912a5cb97057aebfdfd11e4a6b22d8409639ef
UlyssesHu <114092558+UlyssesHu@users.noreply.github.com>
2022-09-22 23:27:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,16 +60,47 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "瑶瑶宝贝",
+      name: "老胡",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oXYx76GCAN3ULJMYphENJ2bX1ius",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "w--6jeTXUtBO8JpNoYqPF6rcL_Jgz0vlEalHgHzrCl4",
+      useTemplateId: "-iWN9iZ6wazInjHdgqnMJUexxiUcEUYdpcmYWfvYecw",
       // 所在省份
       province: "湖北",
       // 所在城市
       city: "武汉",
       // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '09-17',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        //{"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "瑶瑶宝贝", "year": "2000", "date": "02-14"},
+        {"type": "生日", "name": "老胡", "year": "1999", "date": "09-17"},
+        //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2020-02-14"},
+        // 结婚纪念日
+        //{"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+        //{"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+    {
+      name: "瑶瑶宝贝",
+      id: "oXYx76BlSNobV3tnEFI_LkwHuegM",
+      useTemplateId: "-iWN9iZ6wazInjHdgqnMJUexxiUcEUYdpcmYWfvYecw",
+      province: "湖北",
+      city: "武汉",
       horoscopeDate: '02-14',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
@@ -96,18 +127,6 @@ export const USER_CONFIG = {
       ]
     },
     /*{
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
-    {
       name: "老婆2",
       id: "",
       useTemplateId: "",
